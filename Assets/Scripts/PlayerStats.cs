020c6366874efe210fc744deb64602398bd3c08b
Quinton Baudoin <quinton.baudoin@LAF-H2-7D741E.lafa.ad.aie.edu>
2015-10-15 17:12:29 -0500

    pulled my unfinished PlayerStats unity script from the old project

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class PlayerStats : MonoBehaviour
+{
+    [SerializeField]
+    private Slider health;
+    [SerializeField]
+    private Text weaponName;
+    [SerializeField]
+    private Text weaponAmmo;
+
+    // Use this for initialization
+    void Awake()
+    {
+        Messenger.AddListener<float, float>("HealthChange", HealthChange);
+    }
+    void HealthChange(float cur, float max)
+    {
+        health.maxValue = max;
+        health.value = cur;
+    }
+}
