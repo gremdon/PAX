e34dc2cfd4939a42bbe9268e494b4e675ee9a1e6
Dylan Guidry <dylan.guidry@students.aie.edu.au>
2015-10-15 17:28:25 -0500

    Fixed the issues
    Meta files are included this time.

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -40,9 +40,9 @@ public class LevelLoader : Singleton<LevelLoader>
         yield return new WaitForSeconds(.1f);
         while (loading == true)
         {
-            GameObject A = FindObjectOfType<Test>().gameObject;
-            A.transform.position = new Vector3(0, 0, 10);
-            loading = false;
+            //GameObject A = FindObjectOfType<Test>().gameObject;
+            //A.transform.position = new Vector3(0, 0, 10);
+            //loading = false;
         }
 
 
