52fdca47b15cecf4ee0cafd9626337b8589767d9
slanden <slanden523@gmail.com>
2015-10-15 17:02:56 -0500

    standard assets
    import

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+    Transform followTarget;
+    Vector3 offset;
+    Vector3 originPosition;
+    Vector3 originRotation;
+
+    void Start()
+    {
+        originPosition = new Vector3(0.17f, 2.48f, -2.53f);
+        originRotation = new Vector3(18, 0, 0);
+        transform.position = originPosition;
+        transform.eulerAngles = originRotation;
+
+        followTarget = FindObjectOfType<UnityStandardAssets.Characters.ThirdPerson.ThirdPersonUserControl>().transform;
+        offset = transform.position - followTarget.position;
+    }
+
+    void Update()
+    {
+        transform.position = followTarget.position + offset;
+    }
+}
