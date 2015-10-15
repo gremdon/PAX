0b01b78a41704ca90810e90607ca7fd9f2220b8c
Dylan Guidry <dylan.guidry@students.aie.edu.au>
2015-10-15 17:35:46 -0500

    Document the stuff
    #NotMyFault

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -40,9 +40,7 @@ public class LevelLoader : Singleton<LevelLoader>
         yield return new WaitForSeconds(.1f);
         while (loading == true)
         {
-            //GameObject A = FindObjectOfType<Test>().gameObject;
-            //A.transform.position = new Vector3(0, 0, 10);
-            //loading = false;
+            loading = false;
         }
 
 
