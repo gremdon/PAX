cbf87325f077921d03b07aea70e334c79f9e273b
Eric <EricMouledoux@Gmail.com>
2015-10-15 18:28:17 -0500

    spring is broke

--- Assets/Scripts/SpringBoard.cs
+++ Assets/Scripts/SpringBoard.cs
@@ -3,14 +3,13 @@ using System.Collections;
 
 public class SpringBoard : MonoBehaviour
 {
-    void OnTriggerEnter(Collider other)
+    void OnTriggerExit(Collider other)
     {
         if(other.gameObject.GetComponent<Rigidbody>())
         {
-            other.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * power *
-                -other.gameObject.GetComponent<Rigidbody>().velocity.y);
+            other.gameObject.GetComponent<Rigidbody>().AddForce(transform.up * power);
         }
     }
-
+    
     public float power;
 }
