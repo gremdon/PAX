b05a90dc9703e78587aa0ca20c12ffa5ad529bc0
Eric <EricMouledoux@Gmail.com>
2015-10-16 16:04:45 -0500

    pressure plate made

---
+++ Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressurePlate : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if(canPress)
+        {
+            Messenger.Broadcast(messageToPublish.ToLower());
+            canPress = false;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(setDirty)
+        {
+            canPress = true;
+        }
+    }
+
+    private bool canPress = true;
+    public string messageToPublish;
+    public bool setDirty = false;
+}
