10487939e43c0015c3d654f4ef8b5e52fbae2a80
Dylan Guidry <dylan.guidry@students.aie.edu.au>
2015-10-16 16:12:03 -0500

    Pushing to get Eric stuff
    Getting prefabs from eric

---
+++ Assets/Scripts/ComboLock.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ComboLock : MonoBehaviour
+{
+    //Correct Combo to the lock
+    string LockCombo;
+    //Objects that represent a value the user will input as a combo to the lock
+    public GameObject[] Keys;
+    public int[] value; //Value assigned to each key
+
+    ComboLock(string combo)
+    {
+        combo = LockCombo;
+    }
+
+    void UserInput(string s)
+    {
+        
+    }
+}
