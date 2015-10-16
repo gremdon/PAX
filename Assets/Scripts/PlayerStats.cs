96d84c8821e289addd196564bc414f5c1cbb5328
Quinton Baudoin <quinton.baudoin@LAF-H2-7D741E.lafa.ad.aie.edu>
2015-10-16 10:08:13 -0500

    Added hud scene and player stats script for the player stat location.

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -14,6 +14,17 @@ public class PlayerStats : MonoBehaviour
     void Awake()
     {
         Messenger.AddListener<float, float>("HealthChange", HealthChange);
+        Messenger.AddListener<string, int, int>("Weapon Changed",WeaponInfo);
+        Messenger.AddListener<int, int>("Ammo Changed", AmmoCount);
+    }
+    void WeaponInfo(string name, int cur, int max)
+    {
+        weaponName.text = name;
+        weaponAmmo.text = cur.ToString() + '/' + max.ToString();
+    }
+    void AmmoCount(int cur, int max)
+    {
+        weaponAmmo.text = cur.ToString() + '/' + max.ToString();
     }
     void HealthChange(float cur, float max)
     {
