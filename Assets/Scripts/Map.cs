9356f7caed9eae522f95d9e510fa65731f88652b
Chui <lordbabypanda@gmail.com>
2015-10-16 12:46:15 -0500

    figuring out maps

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Map : MonoBehaviour
+{
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            Application.CaptureScreenshot(null, 5);
+        }
+
+    }
+}
