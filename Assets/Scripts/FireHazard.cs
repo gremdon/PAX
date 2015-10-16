217473b4f29a95bcef0848451de016db72eb3116
Zac-King <Zacharyking18@yahoo.com>
2015-10-16 12:01:18 -0500

    Working on Puzzle stage in zac.unity
    +HeatSink Projectile
    +Fire Hazards
    + their interaction with each other
    +HeatSink Projectile Prefab

---
+++ Assets/Scripts/FireHazard.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireHazard : MonoBehaviour
+{
+    void Start ()
+    {
+	    Messenger.AddListener<GameObject>("HeatSink_Hit", OnHeatSinkHit);
+	}
+
+
+    void OnHeatSinkHit(GameObject go)
+    {
+        print("Heatsink Hit received by " + gameObject.name);
+        if(go == gameObject)
+        {
+            Debug.Log(gameObject.name + ": I'm Cool now");
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Messenger.RemoveListener<GameObject>("HeatSink_Hit", OnHeatSinkHit);
+    }
+
+}
