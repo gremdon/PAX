aa81147e36cc9762c925641476cea43aeddbc437
Dylan Guidry <dylan.guidry@students.aie.edu.au>
2015-10-16 11:26:05 -0500

    Puzzle Behaviors
    Setting up moving platforms.

---
+++ Assets/Scripts/MovingPlatforms.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MovingPlatforms : MonoBehaviour
+{
+    public float m_speed;
+    Vector3 InitialPos;
+    public Vector3 TargetPosition;
+
+    IEnumerator MoveObjects()
+    {
+        while(Vector3.Distance(gameObject.transform.position, TargetPosition) > 1.0f)
+        {
+            yield return null;
+        }
+    }
+}
