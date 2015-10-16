fee39e91319f8dcb6f3323072aa20183260ad891
Chui <lordbabypanda@gmail.com>
2015-10-16 16:14:35 -0500

    map

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -5,10 +5,29 @@ public class Map : MonoBehaviour
 {
     public void Update()
     {
+        Camera();
+    }
+    public void Camera()
+    {
         if (Input.GetKeyDown(KeyCode.M))
         {
-            Application.CaptureScreenshot(null, 5);
+            gowCamera.enabled = false;
+            map.enabled = true;
         }
 
+        if (Input.GetKeyUp(KeyCode.M))
+        {
+            gowCamera.enabled = true;
+            map.enabled = false;
+        }
     }
-}
+
+    public Camera gowCamera;
+    public Camera map;
+} 
+
+
+//if (Input.GetKeyDown(KeyCode.M))
+//{
+//    Application.CaptureScreenshot(null, 5);
+//}
