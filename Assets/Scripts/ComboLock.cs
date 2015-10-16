818b6f6b0f05a42d9bb5256708340096722cbe26
Dylan Guidry <dylan.guidry@students.aie.edu.au>
2015-10-16 17:23:25 -0500

    awaiting correct stuff

--- Assets/Scripts/ComboLock.cs
+++ Assets/Scripts/ComboLock.cs
@@ -4,18 +4,65 @@ using System.Collections;
 public class ComboLock : MonoBehaviour
 {
     //Correct Combo to the lock
-    string LockCombo;
+    public string LockCombo;
     //Objects that represent a value the user will input as a combo to the lock
-    public GameObject[] Keys;
-    public int[] value; //Value assigned to each key
+    int j = 0;
+    public int i = 0;
+    int test = 0;
+    public string[] input;
+    string[] value = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+    public GameObject[] keys;
 
-    ComboLock(string combo)
+    void Awake()
     {
-        combo = LockCombo;
+        Callback<string> stuff = UserInput;
+        Messenger.AddListener<string>(value[i], UserInput);
+        Debug.Log(value[i]);
+    }
+
+    void Start()
+    {
+
     }
 
     void UserInput(string s)
     {
-        
+       input[test] = s;
+        test++;
+    }
+
+    [ContextMenu("Add Values")]
+    void SetKeyValues()
+    {
+        foreach(GameObject g in keys)
+        {
+            g.GetComponent<PressurePlate>().messageToPublish = value[i];
+            i += 1;
+        }
     }
 }
+
+
+/*
+//dylans logic
+    messenger.addlistener<int>("walked on combo platform, handleWalkedOn);
+
+    void handleWalkedOn(int id)
+    {
+        debug.log("user walked on " + id);
+
+
+    }
+
+    */
+
+
+    /*
+    platform broadcast
+
+    void OnTriggerEnter()
+    {
+
+    messenger.broadcast<int>("walked on combo platform", this.id);
+    }
+    */
