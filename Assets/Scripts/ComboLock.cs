0b1b72fa3052e10fb2a5d47418ad1d0c10752257
Dylan Guidry <dylan.guidry@students.aie.edu.au>
2015-10-16 17:48:51 -0500

    the lock works just dont run it now you will be fucked

--- Assets/Scripts/ComboLock.cs
+++ Assets/Scripts/ComboLock.cs
@@ -6,63 +6,59 @@ public class ComboLock : MonoBehaviour
     //Correct Combo to the lock
     public string LockCombo;
     //Objects that represent a value the user will input as a combo to the lock
-    int j = 0;
-    public int i = 0;
-    int test = 0;
-    public string[] input;
+    int i = 0;
+    public string input;
     string[] value = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
     public GameObject[] keys;
+    bool checking = false;
 
     void Awake()
     {
         Callback<string> stuff = UserInput;
-        Messenger.AddListener<string>(value[i], UserInput);
-        Debug.Log(value[i]);
+        Messenger.AddListener<string>(name.ToLower(), UserInput);
     }
 
     void Start()
     {
+        SetKeyValues();
+    }
 
+    IEnumerator CheckCombo()
+    {
+        checking = true;
+        while(input != null)
+        {
+            if(input == LockCombo)
+            {
+                Debug.Log("You got it");
+                yield return null;
+            }
+            else if(input.Length >= LockCombo.Length)
+            {
+                Debug.Log("Wrong Mother Fucker");
+            }
+        }
+        checking = false;
+        StopCoroutine(CheckCombo());
     }
 
     void UserInput(string s)
     {
-       input[test] = s;
-        test++;
+        if(checking == false)
+        {
+            StartCoroutine(CheckCombo());
+        }
+        input += s;
     }
 
-    [ContextMenu("Add Values")]
     void SetKeyValues()
     {
         foreach(GameObject g in keys)
         {
-            g.GetComponent<PressurePlate>().messageToPublish = value[i];
+            g.GetComponent<PressurePlate>().stringArgument1 = value[i];
+            g.GetComponent<PressurePlate>().messageToPublish = "Ramp";
+            Debug.Log(value[i]);
             i += 1;
         }
     }
 }
-
-
-/*
-//dylans logic
-    messenger.addlistener<int>("walked on combo platform, handleWalkedOn);
-
-    void handleWalkedOn(int id)
-    {
-        debug.log("user walked on " + id);
-
-
-    }
-
-    */
-
-
-    /*
-    platform broadcast
-
-    void OnTriggerEnter()
-    {
-
-    messenger.broadcast<int>("walked on combo platform", this.id);
-    }
-    */
