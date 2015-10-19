1b31b606c7c4a12b89414c54bdb6be9817bfc283
Chui <lordbabypanda@gmail.com>
2015-10-19 14:48:13 -0500

    Colorful changes.
    Also, the helicopters controls have been changed. Plus Audio

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -1,25 +1,25 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿//using UnityEngine;
+//using System.Collections;
 
-public class Map : MonoBehaviour
-{
-    public void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.M))
-        {
-            OnGUI();
-        }
-    }
+//public class Map : MonoBehaviour
+//{
+//    public void Update()
+//    {
+//        if (Input.GetKeyDown(KeyCode.M))
+//        {
+//            OnGUI();
+//        }
+//    }
 
-    void OnGUI()
-    {
-        GUI.DrawTexture(new Rect(0, 0, 1600, 900), mapImage, ScaleMode.ScaleToFit);
-    }
+//    void OnGUI()
+//    {
+//        GUI.DrawTexture(new Rect(0, 0, 1600, 900), mapImage, ScaleMode.ScaleToFit);
+//    }
 
-    //public Camera mapCamera;
-    public Camera helicopterCamera;
-    public Texture mapImage;
-} 
+//    //public Camera mapCamera;
+//    public Camera helicopterCamera;
+//    public Texture mapImage;
+//} 
 
 
 //if (Input.GetKeyDown(KeyCode.M))
