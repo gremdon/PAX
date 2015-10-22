2a3872af61e6da49ef16e6f4604b8fb7de304a32
Matthew Williamson <chuwilliamson@gmail.com>
2015-10-22 17:56:06 -0500

    Fix issues and merge with dylan
    move populate to editor movableplatform. So we can build

---
+++ Assets/Editor/MoveablePlatformEditor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+
+
+[CustomEditor(typeof(MovablePlatform))]
+public class MovablePlatformEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        MovablePlatform myScript = (MovablePlatform)target;
+        if (GUILayout.Button("Set Initial Posistion"))
+        {
+            myScript.SetInitPos();
+        }
+        if (GUILayout.Button("Set Secondary Posistion"))
+        {
+            myScript.SetSecondaryPos();
+        }
+    }
+}
--- Assets/Scripts/ControllerTest.cs
+++ Assets/Scripts/ControllerTest.cs
@@ -7,7 +7,7 @@ public class ControllerTest : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        Debug.Log(Input.GetJoystickNames()[0]);
+        
     }
 
     void Update()
