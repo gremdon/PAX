438bd4620cc54a29427e9f5dbd2ea1bdc2d6afba
slanden <slanden523@gmail.com>
2015-10-23 10:12:31 -0500

    Error Fixes
    -deleted blue and red materials
    -deleted all material metas and let unity reconstruct them
    -renamed CameraController to PAXCamera due to clashing namespaces.

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Map : MonoBehaviour
+{
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            OnGUI();
+        }
+    }
+
+    void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(0, 0, 1600, 900), mapImage, ScaleMode.ScaleToFit);
+    }
+
+    //public Camera mapCamera;
+    public Camera helicopterCamera;
+    public Texture mapImage;
+}
+
+
+//if (Input.GetKeyDown(KeyCode.M))
+//{
+//    Application.CaptureScreenshot(null, 5);
+//}
--- Assets/Scripts/PAXCamera.cs
+++ Assets/Scripts/PAXCamera.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraController : MonoBehaviour
+public class PAXCamera : MonoBehaviour
 {
     Transform followTarget;
     Vector3 offset;
