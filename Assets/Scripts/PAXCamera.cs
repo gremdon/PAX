68bb13a24b3b01df7797a28ee9637aba7cdb600d
slanden <slanden523@gmail.com>
2015-10-23 14:36:01 -0500

    Camera
    -camera accommodates for min and max distances to player

--- Assets/Scripts/PAXCamera.cs
+++ Assets/Scripts/PAXCamera.cs
@@ -4,13 +4,13 @@ using System.Collections;
 public class PAXCamera : MonoBehaviour
 {
     Transform followTarget;
-    Vector3 offset;
     public float distance = 2.2f;
     public float minDistance = 3f;
     public float maxDistance = 10f;
     public float smoothing = .1f;
-    public Vector3 originPosition = new Vector3(0, 1.8f, 0);
-    Vector3 originRotation;
+    //public Vector3 originPosition = new Vector3(0, 1.8f, 0);
+    //Vector3 originRotation;
+    Vector3 offset;
     Vector3 cameraLookDirection;
     Vector3 playerMoveDirection;
 
@@ -20,17 +20,10 @@ public class PAXCamera : MonoBehaviour
         followTarget = FindObjectOfType<UnityStandardAssets.Characters.ThirdPerson.ThirdPersonUserControl>().transform;
 
         //set initial camera position and orientation
-        originPosition = followTarget.position;
-        originPosition += (followTarget.forward * distance);
-        originPosition += new Vector3(0, 3, 0);
-
-        transform.position = originPosition;
+        transform.position = followTarget.position + (followTarget.forward * distance) + new Vector3(0, 3, 0);
         transform.LookAt(followTarget);
         cameraLookDirection = transform.forward;
-        //originPosition = new Vector3(0.17f, 2.48f, -2.53f);
-        //originRotation = new Vector3(18, 0, 0);
 
-        //transform.eulerAngles = originRotation;
         //Reset();
         offset = transform.position - followTarget.position;
 
@@ -39,23 +32,38 @@ public class PAXCamera : MonoBehaviour
     void Update()
     {
         Vector3 v = followTarget.position - transform.position;
-        if (/* Vector3.Distance( hit.point, transform.position)*/ (v.x * v.x + v.z * v.z) <= minDistance * minDistance)
+        if ((v.x * v.x + v.z * v.z) <= minDistance * minDistance || 
+            (v.x * v.x + v.z * v.z) >= maxDistance * maxDistance)
         {
-            //transform.position = followTarget.position + offset;
             transform.position = Vector3.Lerp(transform.position, followTarget.position + offset, Time.deltaTime * smoothing);
         }
-        else if((v.x * v.x + v.z * v.z) >= maxDistance * maxDistance)
-        {
-            transform.position += followTarget.position + offset;
-            //transform.forward = Vector3.Lerp(transform.forward, transform.LookAt(followTarget), Time.deltaTime * smoothing);
-        }
+        //else if((v.x * v.x + v.z * v.z) >= maxDistance * maxDistance)
+        //{
+        //    transform.position = followTarget.position + offset;
+        //    //transform.forward = Vector3.Lerp(transform.forward, transform.LookAt(followTarget), Time.deltaTime * smoothing);
+        //}
         transform.LookAt(followTarget);
+    }
+
+    void PredictPosition()
+    {
+        Vector3 predictedCameraPosition = transform.position + (followTarget.forward * distance);
+
+        RaycastHit hit;
+        Ray r = new Ray(predictedCameraPosition, transform.forward);
+
+        Physics.Raycast(r, out hit);
+        //float distanceSquared = followTarget.position.x (hit.point.x * hit.point.x + hit.point.y * hit.point.y + hit.point.z * hit.point.z);
+        //if(Vector3.Distance(hit.point, predictedCameraPosition) <= Vector3.Distance()
+        //{
+
+        //}
     }
 
     void Reset()
     {
         transform.position = (followTarget.position - followTarget.forward) * distance;
-        transform.position += originPosition;
+        //transform.position += originPosition;
         transform.eulerAngles = new Vector3(18, 0, 0) + followTarget.eulerAngles;
 
         offset = transform.position - followTarget.position;
