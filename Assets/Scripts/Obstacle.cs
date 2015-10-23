8eea7e6ddbce96fd9e0e2a907a783f9d2847fb2f
Matthew Williamson <chuwilliamson@gmail.com>
2015-10-23 15:53:29 -0500

    a
    a

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -5,9 +5,13 @@ public class Obstacle : HealthStats
 {
     protected override void OnDead()
     {
-        gameObject.SetActive(false);
-        Destroy(gameObject);
+        GetComponent<Rigidbody>().isKinematic = false;
+        float yoda = 5000f;
+        GetComponent<Rigidbody>().AddForce(yoda * Vector3.up);
+        //gameObject.SetActive(false);
+        //Destroy(gameObject);
     }
+
 
 }
 /// Eric Mouledoux
