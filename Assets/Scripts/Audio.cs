b9c72a69a52b489292b0cade62689431cf8fd759
Chui <lordbabypanda@gmail.com>
2015-10-23 14:12:56 -0500

    Audio Comments

--- Assets/Scripts/Audio.cs
+++ Assets/Scripts/Audio.cs
@@ -7,21 +7,30 @@ using System.Collections.Generic;
 /// Tran did all of the audio script.
 /// While Paul is looking into 3D settings.
 /// </summary>
-public class Audio
+static public class Audio
 {
     /// <summary>
-    /// A base audio for everyone to play the audio.
-    /// A dictionary to add the audio
+    /// Audio Manager to play audio
     /// </summary>
-    public AudioSource audioManager;
-    Dictionary<string, AudioClip> audioTable = new Dictionary<string, AudioClip>();
+    static public AudioSource audioManager;
 
     /// <summary>
-    /// Adds audio, checks to see if the same message is in the dictionary, if not, adds it in.
+    /// Key : string arg
+    /// Value : AudioClip
+    /// play audio depend on the string arg
     /// </summary>
-    /// <param string ="argument"></param>
-    /// <param AudioClip ="audioclip"></param>
-    public void AddAudio(string argument, AudioClip audioclip)
+    static Dictionary<string, AudioClip> audioTable = new Dictionary<string, AudioClip>();
+
+    /// <summary>
+    /// Add AudioClip with string argument to Dictionary
+    /// </summary>
+    /// <param name ="argument">
+    /// Argument that audio need to play to
+    /// </param>
+    /// <param name ="audioclip">
+    /// Audioclip that plays with the assign argument
+    /// </param>
+    static public void AddAudio(string argument, AudioClip audioclip)
     {
         if (audioTable.ContainsKey(argument))
         {
@@ -32,21 +41,23 @@ public class Audio
     }
 
     /// <summary>
-    /// 
+    /// Subscribing to broadcast message that audio will listen to
     /// </summary>
     /// <param name="messageBroadcast">
-    /// The message that the audio wants to listen to
+    /// The message that is interested in.
     ///</param>
-    public void AudioListener(string messageBroadcast)
+    static public void AudioListener(string messageBroadcast)
     {
         Messenger.AddListener<string>(messageBroadcast, playAudio);
     }
 
     /// <summary>
-    /// The audio plays by string arguments.
+    /// Play the audio
     /// </summary>
-    /// <param string="argument"></param>
-    void playAudio(string argument)
+    /// <param name="argument">
+    /// Argument that audio needs to play to. :3
+    /// </param>
+    static private void playAudio(string argument)
     {
         audioManager.clip = audioTable[argument];
         audioManager.Play();
