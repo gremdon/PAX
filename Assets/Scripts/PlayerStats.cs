a618b9fdc2a2be32a082bb2e21a0edd3935a7eae
Quinton Baudoin <quinton.baudoin@LAF-H2-7D741E.lafa.ad.aie.edu>
2015-10-23 10:06:01 -0500

    Added some comments

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -1,5 +1,22 @@
-﻿using UnityEngine;
+﻿/// <summary>
+/// Developed by: Quinton "Kiro" Baudoin.
+/// 
+/// Purpose: To act as a health counter.
+/// 
+/// Usage: Attach to desired gameobject and use the Messanger System
+///         to broadcast "takedamage", with name of gameobject and this
+///         script will check to see if it got hit and then reduce health.
+///         When health hits 0 or less "playerdied" with name of gameobject this is
+///         attached to.
+/// 
+/// </summary>
+///////////////////////////////////
+
+
+using UnityEngine;
 using System.Collections;
+
+
 
 public class PlayerStats : MonoBehaviour
 {
@@ -28,7 +45,7 @@ public class PlayerStats : MonoBehaviour
 
     private void OnDead()
     {
-        Messenger.Broadcast("playerdied", this);
+        Messenger.Broadcast("playerdied", name);
     }
 
     public void OnEnable()
