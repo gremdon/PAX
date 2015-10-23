dcfc7d7b07987db55b66174ce942dacf6a7f37cc
slanden <slanden523@gmail.com>
2015-10-23 18:49:44 -0500

    camera
    party camera

---
+++ Assets/Scripts/BetweenPlayer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BetweenPlayer : MonoBehaviour {
+
+    public float zoffset;
+    public Transform player1;
+    public Transform player2;
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = new Vector3(
+            player1.position.x + player2.position.x, 
+            player1.position.y + player2.position.y, 
+            player1.position.z + player2.position.z + zoffset)/2.0f;
+	
+	}
+}
