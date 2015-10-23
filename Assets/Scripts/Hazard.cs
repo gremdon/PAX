df8da2b305a48382bb8e61259734e35717d336f8
Eric <EricMouledoux@Gmail.com>
2015-10-23 09:55:51 -0500

    hazards now use coroutines to deal damage over time

--- Assets/Scripts/Hazard.cs
+++ Assets/Scripts/Hazard.cs
@@ -5,7 +5,23 @@ public class Hazard : MonoBehaviour
 {
     void OnTriggerEnter(Collider other)
     {
-        Messenger.Broadcast("takedamage", other.name);
-        print("takedamage" + other.name);
+        StartCoroutine("TakeDamage", other);
     }
+
+    void OnTriggerExit(Collider other)
+    {
+        StopCoroutine("TakeDamage");
+    }
+
+    IEnumerator TakeDamage(Collider other)
+    {
+        while (true)
+        {
+            Messenger.Broadcast("takedamage", other.name);
+            yield return new WaitForSeconds(timer);
+        }
+    }
+
+    [SerializeField]
+    float timer = 1;
 }
