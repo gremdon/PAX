ac863c137e59606fc5e23050d017e622986f3009
Matthew Williamson <chuwilliamson@gmail.com>
2015-10-23 13:43:56 -0500

    level and code from eric

---
+++ Assets/Scripts/BroadcastOnTrigger.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BroadcastOnTrigger : MonoBehaviour {
+    public string message;
+    void OnTriggerEnter(Collider col)
+    {
+        Messenger.Broadcast<string>(message, col.name);
+    }
+}
