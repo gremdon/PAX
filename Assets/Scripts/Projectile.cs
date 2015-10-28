8777f6a0f0f4a66508b5395a7d48be5f6cef7674
Quinton Baudoin <quinton.baudoin@LAF-H2-7D741E.lafa.ad.aie.edu>
2015-10-28 16:48:03 -0500

    Projectile

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour
+{
+    
+
+    [SerializeField]
+    private int speed = 5;
+    [SerializeField]
+    private float Timer = 100;
+
+    bool alive = true;
+
+    // Use this for initialization
+
+    // Update is called once per frame
+    void Start()
+    {
+        if (gameObject.GetComponent<Rigidbody>())
+          GetComponent<Rigidbody>().AddForce(transform.forward * speed);
+        
+        else StartCoroutine("Forward");
+    }
+
+    IEnumerator Forward()
+    {        
+        transform.position += transform.forward * speed;
+
+        yield return null;
+    }
+
+    void Update()
+    {
+       
+        Timer -= Time.deltaTime;
+        if (Timer < 0)
+        OnCollisionEnter();
+    }
+
+    void OnCollisionEnter()
+    {
+        if (!gameObject.GetComponent<Rigidbody>())
+            StopCoroutine("Forward");
+
+        Destroy(gameObject);
+    }
+
+
+}
