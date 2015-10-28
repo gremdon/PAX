3da75d74666c295e823c1e342144c88e79165cd7
Eric <EricMouledoux@Gmail.com>
2015-10-28 18:31:03 -0500

    Enemies
    done ruined it

--- Assets/Scripts/RigidWander.cs
+++ Assets/Scripts/RigidWander.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+[RequireComponent(typeof(Rigidbody))]
 public class RigidWander : MonoBehaviour
 {
     void Start()
@@ -14,23 +14,39 @@ public class RigidWander : MonoBehaviour
     {
         if (canMove)
         {
-            rb.AddForce((nxtPos - transform.position).normalized * speed);
+            rb.AddForce((transform.forward).normalized * speed);
         }
-
-        if(Vector3.Distance(transform.position, nxtPos) < 1f)
+        float closingDistance = .5f;
+        float distance = Vector3.Distance(transform.position, nxtPos);
+        txt.text = distance.ToString();
+        if ( distance < closingDistance ) //if i'm close
         {
+            //get next pos and then turn
             SetNextPos();
         }
+
+       // Debug.Log(nxtPos.magnitude);
+       // Debug.Log(nxtPos);
+       
+        Debug.DrawLine(transform.position, nxtPos, Color.red);
+
+        Debug.DrawLine(transform.position, transform.forward.normalized, Color.blue);
+
+        Debug.Log(nxtPos.normalized);
+        Debug.DrawLine(transform.position, Vector3.Normalize(transform.position - nxtPos), Color.black);
     }
 
     IEnumerator Turn()
     {
-        while(Vector3.Distance(transform.forward, nxtPos.normalized) > 1)
+        float dot = Vector3.Dot(transform.forward, Vector3.Normalize(transform.position - nxtPos));
+        
+        while ( dot < .75f )
         {
-            transform.rotation =
-                Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(nxtPos), Time.deltaTime);
+            Debug.Log("dot is " + dot);
+            transform.forward += (transform.forward + nxtPos) * Time.deltaTime;
             yield return null;
         }
+        Debug.Log("done turning");
         canMove = true;
     }
 
@@ -47,7 +63,14 @@ public class RigidWander : MonoBehaviour
     public float range;
 
     bool canMove;
+
+    float stuckTimer;
+    float stuckDelay;
+
     Vector3 nxtPos;
     Vector3 origin;
+
     Rigidbody rb;
+
+    public UnityEngine.UI.Text txt;
 }
