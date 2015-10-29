da13ec08fee8edf04a969eb4e99807c24ca02a74
Eric <EricMouledoux@Gmail.com>
2015-10-29 12:13:19 -0500

    Enemies
    removed broken enemy prefab. new one in scene eric and.

--- Assets/Scripts/RigidWander.cs
+++ Assets/Scripts/RigidWander.cs
@@ -7,70 +7,38 @@ public class RigidWander : MonoBehaviour
     {
         origin = transform.position;
         rb = GetComponent<Rigidbody>();
-        SetNextPos();
+        SetNextPosition();
     }
-
+    Vector3 f;
     void Update()
     {
-        if (canMove)
-        {
-            rb.AddForce((transform.forward).normalized * speed);
-        }
-        float closingDistance = .5f;
-        float distance = Vector3.Distance(transform.position, nxtPos);
-        txt.text = distance.ToString();
-        if ( distance < closingDistance ) //if i'm close
-        {
-            //get next pos and then turn
-            SetNextPos();
-        }
-
-       // Debug.Log(nxtPos.magnitude);
-       // Debug.Log(nxtPos);
-       
-        Debug.DrawLine(transform.position, nxtPos, Color.red);
-
-        Debug.DrawLine(transform.position, transform.forward.normalized, Color.blue);
-
-        Debug.Log(nxtPos.normalized);
-        Debug.DrawLine(transform.position, Vector3.Normalize(transform.position - nxtPos), Color.black);
-    }
-
-    IEnumerator Turn()
-    {
-        float dot = Vector3.Dot(transform.forward, Vector3.Normalize(transform.position - nxtPos));
-        
-        while ( dot < .75f )
-        {
-            Debug.Log("dot is " + dot);
-            transform.forward += (transform.forward + nxtPos) * Time.deltaTime;
-            yield return null;
-        }
-        Debug.Log("done turning");
-        canMove = true;
-    }
-
-    void SetNextPos()
-    {
-        canMove = false;
         rb.velocity = Vector3.zero;
+        rb.AddForce((nxtPos - transform.position).normalized * 100 * speed);
+
+        if(Vector3.Distance(transform.position, nxtPos) < 1f)
+            SetNextPosition();
+        
+
+        Debug.DrawLine(transform.position, nxtPos, Color.red);
+    }
+
+    void SetNextPosition()
+    {
+        lstPos = nxtPos;
         Vector2 t = (Random.insideUnitCircle * range);
-        nxtPos = new Vector3(t.x, 0, t.y) + (origin - transform.position);
-        StartCoroutine("Turn");
+        nxtPos = new Vector3(t.x, 0, t.y) + origin;
     }
 
     public float speed;
     public float range;
 
+    public Vector3 nxtPos = new Vector3();
+
     bool canMove;
 
-    float stuckTimer;
-    float stuckDelay;
-
-    Vector3 nxtPos;
-    Vector3 origin;
+    Vector3 lstPos = new Vector3();
+    Vector3 origin = new Vector3();
+    Vector3 forward = new Vector3();
 
     Rigidbody rb;
-
-    public UnityEngine.UI.Text txt;
 }
