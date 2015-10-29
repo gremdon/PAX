e07ecda4d98600cdce1d45687396507f5e7c18ae
Quinton Baudoin <quinton.baudoin@LAF-H2-7D741E.lafa.ad.aie.edu>
2015-10-29 17:34:09 -0500

    Created cannons for the Projectiles

---
+++ Assets/Scripts/AutoCannon.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoCannon : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject projectilePref;
+    [SerializeField]
+    private float delay;
+
+
+    float timer = 0;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(delay<timer)
+        {
+            Instantiate(projectilePref, transform.position, transform.rotation);
+
+            timer = 0;
+        }
+
+        timer += Time.deltaTime;
+
+    }
+}
