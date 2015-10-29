2f25ab2730caad3283f85eb2db442a447cd49482
Matthew Williamson <chuwilliamson@gmail.com>
2015-10-29 17:01:40 -0500

    ui
    sliders for player ui hot lava

---
+++ Assets/Scripts/CameraListener.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraListener : MonoBehaviour {
+    public string listeningFor;
+	// Use this for initialization
+	void Awake()
+    {
+        Messenger.AddListener<string>(listeningFor, SetCam);
+        gameObject.SetActive(false);
+    }
+
+    void SetCam(string s)
+    {
+        gameObject.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/SliderChange.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SliderChange : HudListener {
+
+    protected override void DoSomething(string message)
+    {
+        GetComponent<Slider>().value--;
+    }
+}
