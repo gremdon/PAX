ba066904d7d969f505c0cb9ec14c81ad107ff47d
Chui <lordbabypanda@gmail.com>
2015-10-30 12:15:31 -0500

    Audio
    Ambient Audio, BACK GROUND MUSIC!!!!

---
+++ Assets/Scripts/AmbientAudio.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AmbientAudio : MonoBehaviour
+{
+    public AudioSource backgroundAudioSource;
+    public AudioClip backgroundSound;
+
+    void Awake()
+    {
+        backgroundAudioSource = GetComponent<AudioSource>();
+        Audio.audioManager = backgroundAudioSource;
+        Audio.AmbientSound(backgroundSound);
+    }
+
+}
