86aea5211873652aaf5493218ae268745468d7a7
slanden <slanden523@gmail.com>
2015-10-30 16:00:44 -0500

    Camera
    -changed the way cameras swap

--- Assets/Scripts/BroadcastOnTrigger.cs
+++ Assets/Scripts/BroadcastOnTrigger.cs
@@ -3,9 +3,9 @@ using System.Collections;
 
 public class BroadcastOnTrigger : MonoBehaviour {
     public string message;
-    void OnTriggerEnter(Collider col)
+    protected virtual void OnTriggerEnter(Collider col)
     {
         //col is the name of the object that enters the trigger
-        Messenger.Broadcast<string, string>(message, col.name,this.name);
+        Messenger.Broadcast<string>(message, col.name);
     }
 }
