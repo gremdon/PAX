91017f511dde851a77c7b07176f8f864881cc44f
Chui <lordbabypanda@gmail.com>
2015-11-02 09:49:56 -0600

    updating from Matthew

--- Assets/Scripts/AmbientAudio.cs
+++ Assets/Scripts/AmbientAudio.cs
@@ -3,23 +3,14 @@ using System.Collections;
 
 public class AmbientAudio : MonoBehaviour
 {
-    public AudioSource ambientAudioAS;
+    public AudioSource backgroundAudioSource;
     public AudioClip backgroundSound;
-    //public AudioClip backgroundSound2;
 
     void Awake()
     {
-        ambientAudioAS = GetComponent<AudioSource>();
-        ambientAudioAS.clip = backgroundSound;
-        ambientAudioAS.playOnAwake = true;
-        ambientAudioAS.loop = true;
-        ambientAudioAS.priority = 256;
-        ambientAudioAS.volume = 0.1f;
-        ambientAudioAS.Play();
+        backgroundAudioSource = GetComponent<AudioSource>();
+        AudioManager.audioSource = backgroundAudioSource;
+        AudioManager.AmbientSound(backgroundSound);
     }
 
-    public void ChangeAmbientAudio()
-    {
-
-    }
 }
